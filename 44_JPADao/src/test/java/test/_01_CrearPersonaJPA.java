c33faec2f30050b6033a8401ee402c1da84630f2
fdepablo <fdepablo@gmail.com>
2020-01-17 14:20:24 +0100

    proyecto jpa

---
+++ 44_JPADao/src/test/java/test/_01_CrearPersonaJPA.java
@@ -0,0 +1,20 @@
+package test;
+
+import modelo.entidad.Persona;
+import modelo.persistencia.PersonaDao;
+
+public class _01_CrearPersonaJPA {
+	public static void main(String[] args) {
+		PersonaDao pd = new PersonaDao();
+		
+		Persona persona = new Persona();
+		persona.setNombre("Bud Spencer Dao");
+		persona.setEdad(45);
+		persona.setPeso(125);
+		
+		int id = pd.insertar(persona);
+		System.out.println(id);
+		
+		System.out.println("Persona creada");
+	}
+}
